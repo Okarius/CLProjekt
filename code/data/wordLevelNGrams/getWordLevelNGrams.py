b30a9000c9e1ce17f5c87ed143408a345c8e7d7c
Okarius <nikolas.zeitler@student.uni-tuebingen.de>
2016-09-02 15:22:35 +0200

    minor Fixes nGramsWordLevel

--- code/data/wordLevelNGrams/getWordLevelNGrams.py
+++ code/data/wordLevelNGrams/getWordLevelNGrams.py
@@ -3,25 +3,28 @@ import sys
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('utf-8')
 
-tweets = pd.read_csv('data/outOffset.tsv', sep='\t', encoding='utf-8')
-tweetStrings = []
-unigrams = set()
-for i in range(len(tweets)):
-    tweet = str(tweets["tweet"][i]).decode("utf-8")
-    if "Not Found" not in tweet: #some tweets are Not Found
-        unigrams = unigrams | set(tweet)
+#tweets = pd.read_csv('../outOffset.tsv', sep='\t', encoding='utf-8')
+#tweetStrings = []
+#unigrams = set()
+#for i in range(len(tweets)):
+#    tweet = str(tweets["tweet"][i]).decode("utf-8")
+#    if "Not Found" not in tweet: #some tweets are Not Found
+#        unigrams = unigrams | set(tweet)
         
         
-unigramsList = list(unigrams)
-f = open("unigramsWordLevel.txt", "w")
-for i in range(len(unigrams)):
-    f.write(unigrams.pop()+ "\n")
+#unigramsList = list(unigrams)
+#f = open("unigramsWordLevel.txt", "w")
+#for i in range(len(unigrams)):
+#    f.write(unigrams.pop()+ "\n")
+#f.close()
+f = open("unigramsWordLevel.txt","r")
+unigrams = f.readlines()
+unigrams =[i.replace("\n","") for i in unigrams]
 f.close()
 
-
 f = open("bigramsWordLevel.txt", "w")
-for u in unigramsList:
-    for utwo in unigramsList:
+for u in unigrams:
+    for utwo in unigrams:
         f.write( u + utwo + "\n")
 f.close()
 
@@ -29,8 +32,8 @@ f.close()
 
 
 f = open("trigramsWordLevel.txt", "w")
-for u in unigramsList:
-    for utwo in unigramsList:
-            for uthree in unigramsList:
+for u in unigrams:
+    for utwo in unigrams:
+            for uthree in unigrams:
                 f.write( u + utwo + uthree + "\n")
 f.close()
