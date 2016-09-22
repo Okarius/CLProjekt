8d35dc1b054832d25375fcb5fd15c5bb5a10838f
Mihael Simonic <smihael@gmail.com>
2016-09-22 11:18:59 +0200

    multigram tables need special char unigrams

--- code/create_multigram_tables.py
+++ code/create_multigram_tables.py
@@ -1,24 +1,42 @@
 # -*- coding: utf-8 -*-
+
 #Table to create the bigram table for our trainingsdata
 import pandas as pd
 import numpy as np
 import sys
-reload(sys)  # Reload does the trick!
-sys.setdefaultencoding('utf-8')
 
-#ReadAllFiles
-tweets = pd.read_csv('../../../data/data.tsv', sep='\t', encoding='utf-8')
-usefullUnigrams = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+#uncomment for systems with non-utf8 default locale
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+# read files
+tweets = pd.read_csv('../data/data.tsv', sep='\t', encoding='utf-8')
+
+
+# features definitions
+unigrams= [".",")","!","@","#","+","-","*","'",":",";","^",">","<","|","(",
+          "\"","[","]","…","☆","♡","&","“",",","$","","_","ə","ɛ","ʌ","ʃ",
+          "á", "é", "í", "ó", "ú","ü","ñ", "¿",
+          "1","2","3","4","5","6","7","8","9","0"]
+
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                   "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 bigrams =[]
-for i in range(len(usefullUnigrams)):
-    for j in range(len(usefullUnigrams)):
-        bigrams.append(usefullUnigrams[i]+usefullUnigrams[j])
+for i in range(len(letters)):
+    for j in range(len(letters)):
+        bigrams.append(letters[i]+letters[j])
+        
+        
 spanishTrigrams=["que","ent","nte","con","est","ado","par","los","era",
                  "ien","men","per","sta","ara","una","por"]
+
 englishTrigrams=["the","and","tha","ent","ing","ion","tio","for","nde",
                  "has","nce","edt","tis","oft","sth","men"]
 
-multigrams = usefullUnigrams + bigrams+ spanishTrigrams+englishTrigrams
+multigrams = unigrams + letters + bigrams + spanishTrigrams + englishTrigrams
+
+
 #GetListsWithContent
 tweetsString =[] 
 for s in tweets["tweetString"]:
@@ -33,7 +51,7 @@ for s in tweets["taggedLanguages"]:
     languageArray.append(s.split(" "))
 
 
-#get all Words    
+#get all words    
 wordSet = set()
 for s in tweetsArray:
     wordSet = wordSet | set(s)
@@ -54,7 +72,7 @@ for word in wordList:
     
     
     
-#check if Unigram in word.  
+#check if unigram in word.  
 hasUniGramDic = {}
 for word in wordList:
     multigramsList = []
@@ -65,12 +83,12 @@ for word in wordList:
             multigramsList.append(0)
     hasUniGramDic[word] = multigramsList
 
-#buildTable
+#build table
 colomns=["word"] + multigrams + ["label"]
 data = []
 for word in wordList:
     data.append([word] + hasUniGramDic[word] + [findLanguage(word)])
 table= pd.DataFrame(data=data, columns=colomns)    
-table.to_csv("wordLevelAbsolutMultiGramTable.csv", sep='\t')
+table.to_csv("../data/wordLevelAbsolutMultiGramTable.csv", sep='\t')
 
 
